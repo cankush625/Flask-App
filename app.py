168ad2d6ee1cd0038c4a3baa52257af366406847
cankush625 <cankush625@gmail.com>
2021-03-10 01:36:58 +0530

    Code for rendering the html templates using Jinja2 templating
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+import subprocess as sp
 
 app = Flask("myapp")
 
 @app.route("/")
 def my_home():
-    print("Welcome to my home")
-    return "home"
+    date = sp.getoutput("date /t")
+    return render_template("home.html", date=date)
